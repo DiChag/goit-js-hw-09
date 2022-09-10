b16e3f8c718de1fe294c2ce522141410af078fb1
DiChag <chagindmitry1@gmail.com>
2022-09-11 01:37:41 +0300

    1-task-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+let intervalId = null;
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', onBtnStart);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+  intervalId = setInterval(colorSwitcher, 1000);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function colorSwitcher() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
